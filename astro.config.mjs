5de2552b0adf7207106c743a861b1e3fc3f8f7c8
YOSHIDA Takumi <a.sassa92@gmail.com>
2024-04-23 11:27:44 +0900

    GithubPagesで発行されるURLに合わせてbase設定とリンク修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://VHEmv6w7cyLAR7gs8KqG",
-  integrations: [preact()]
+  base: "/astro-pages",
+  site: "https://aiyzatt.github.io/astro-pages/",
+  integrations: [preact()],
 });
